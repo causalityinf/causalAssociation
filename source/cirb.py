26aea8427b876a6c084ea50db1d3711cd3af6b61
causalityinf <99279253+causalityinf@users.noreply.github.com>
2023-01-17 18:08:18 +0100

    add

--- source/cirb.py
+++ source/cirb.py
@@ -4,19 +4,6 @@ from cirb_duration_data_object import CIRBDurationDataObject
 def lambda_(
     data_obj: CIRBDurationDataObject, cause: str, effect: str, window_size: int
 ):
-    """
-    Compute:
-              w        sum_duration_y_in_window(Nw(y <- x))
-        lambda    = -----------------------------------------
-              x|y           total_duration(y)
-
-        In which:
-            sum_duration_y_in_window(Nw(y <- x)):
-                Given x occurs and y occurred in the previous window,
-                accumulate the durations of all y that window.
-
-            total_duration(y): All durations of y in the entire dataset.
-    """
     sum_duration_y_in_window = data_obj.accumulated_cause_durations[
         (cause, effect, window_size)
     ]
@@ -32,15 +19,6 @@ def lambda_(
 def cirb(
     data_obj: CIRBDurationDataObject, cause: str, effect: str, window_size: int
 ) -> float:
-    """
-    Compute:
-                            w
-                      lambda
-                            y|x
-        CIRb(x, y) = -----------
-                      lambda
-                            y
-    """
     nominator = lambda_(data_obj, cause, effect, window_size)
     denominator = data_obj.N[effect] / data_obj.T
 
